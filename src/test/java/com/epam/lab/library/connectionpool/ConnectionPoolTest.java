96556e2af11d6f01d70c8b25c1c052344c59b821
rabaev <rabaev@phoenixit.ru>
2019-07-06 22:30:13 +0300

    feat(cp): add basic connection pool

---
+++ src/test/java/com/epam/lab/library/connectionpool/ConnectionPoolTest.java
@@ -0,0 +1,22 @@
+package com.epam.lab.library.connectionpool;
+
+import org.junit.Test;
+
+public class ConnectionPoolTest {
+
+    @Test
+    public void retrieveConnection() throws Exception {
+
+    }
+
+    @Test
+    public void releaseConnection() throws Exception {
+
+    }
+
+    @Test
+    public void getSize() throws Exception {
+
+    }
+
+}
