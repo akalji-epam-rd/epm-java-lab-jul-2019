296f18881157c8654bdc7b3300f49fdfdb41d48f
rabaev <rabaev@phoenixit.ru>
2019-07-08 15:56:34 +0300

    feat(cp): add basic connection pool

--- src/test/java/com/epam/lab/library/connectionpool/ConnectionPoolTest.java
+++ src/test/java/com/epam/lab/library/connectionpool/ConnectionPoolTest.java
@@ -11,23 +11,22 @@ public class ConnectionPoolTest {
     @Test
     public void getConnection() throws Exception {
 
-        assert(cp.getConnection() != null);
 
-    }
+        assert (cp.getConnection() != null);
 
-    @Test
-    public void releaseConnection() throws Exception {
 
     }
 
     @Test
     public void getSize() throws Exception {
 
+
         int cpSize = cp.getSize();
         Connection connection = cp.getConnection();
-        assert(cp.getSize() == cpSize - 1);
+        assert (cp.getSize() == cpSize - 1);
         cp.releaseConnection(connection);
-        assert(cp.getSize() == cpSize);
+        assert (cp.getSize() == cpSize);
+
 
     }
 
