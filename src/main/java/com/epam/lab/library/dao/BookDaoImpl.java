2b142108b5cd4fe304480f8896348afdb3be59d6
Kirill_Grishin <dji.key@inbox.ru>
2019-07-15 15:52:09 +0300

    ViewResolver blank and view example added

--- src/main/java/com/epam/lab/library/dao/BookDaoImpl.java
+++ src/main/java/com/epam/lab/library/dao/BookDaoImpl.java
@@ -14,6 +14,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+//TODO put ResultSet in try with resources
 public class BookDaoImpl implements BookDao {
 
     private ConnectionPool pool = ConnectionPool.getInstance();
