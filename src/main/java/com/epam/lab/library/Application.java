6b263151b5934b6ad5f3830139bc3fbc5c22a224
Kirill_Grishin <dji.key@inbox.ru>
2019-07-23 14:38:53 +0300

    Pre final

--- src/main/java/com/epam/lab/library/Application.java
+++ src/main/java/com/epam/lab/library/Application.java
@@ -1,17 +1,8 @@
 package com.epam.lab.library;
 
 
-import com.epam.lab.library.domain.Author;
-import com.epam.lab.library.domain.Book;
-import com.epam.lab.library.service.AuthorService;
-import com.epam.lab.library.service.BookService;
-
-import java.util.List;
-
 public class Application {
 
     public static void main(String[] args) {
-        AuthorService service = new AuthorService();
-        List<Author> authors = service.findByLastName("ozd");
     }
 }
